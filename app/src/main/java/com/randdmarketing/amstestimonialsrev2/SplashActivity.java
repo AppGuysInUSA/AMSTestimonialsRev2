c7e9b9bdf80fcc77ac8627e146f8457e3242ef72
Scott <scottthomas3000@gmail.com>
2015-07-23 20:55:33 -0400

    Splash screen added with WiFi detection Also added rounded corners

--- app/src/main/java/com/randdmarketing/amstestimonialsrev2/SplashActivity.java
+++ app/src/main/java/com/randdmarketing/amstestimonialsrev2/SplashActivity.java
@@ -6,9 +6,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -39,9 +37,6 @@ public class SplashActivity extends Activity {
         } else {
 
             Toast.makeText(getBaseContext(), "Please make sure you are on WI-FI", Toast.LENGTH_LONG).show();
-            //Not sure if this is needed -----> Log.v("Home", "############################You are not online!!!!");
-            //Intent i = new Intent(SplashActivity.this, MainActivity.class);
-            //startActivity(i);
             TextView wifiMsg = (TextView)findViewById(R.id.wifiMsg);
             wifiMsg.setText("WIFI IS NOT ENABLED \r\n OR NOT ONLINE \r\n\n If not on WiFi Network \r\n Data Charges may incur when submitting.");
 
