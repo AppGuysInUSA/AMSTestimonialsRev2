86d583de31adbe0f0d4b8c0a3c66f988291725f5
Scott <scottthomas3000@gmail.com>
2015-07-24 22:08:22 -0400

    Tweaked the WiFi detection code

--- app/src/main/java/com/randdmarketing/amstestimonialsrev2/SplashActivity.java
+++ app/src/main/java/com/randdmarketing/amstestimonialsrev2/SplashActivity.java
@@ -36,7 +36,6 @@ public class SplashActivity extends Activity {
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
         // If WiFi is turned on and is connected to Network...erythang all good
-
         if (userWifi.isConnected()) {
             TextView wifiMsg = (TextView) findViewById(R.id.wifiMsg);
             wifiMsg.setText("WIFI ENABLED AND CURRENTLY ONLINE");
@@ -47,12 +46,12 @@ public class SplashActivity extends Activity {
         }
 
         // If WiFi is not turned on... turn it on
-
         if (wifi.setWifiEnabled(false)) {
             //-Toast.makeText(getBaseContext(), "Please make sure you are on WI-FI", Toast.LENGTH_LONG).show();
             TextView wifiMsg = (TextView) findViewById(R.id.wifiMsg);
             wifiMsg.setText("WIFI IS NOT ENABLED \r\n Click above to turn on Wi-Fi");
             wifiLogo.setVisibility(View.VISIBLE);
+            enterBtn.setVisibility(View.VISIBLE);
 
             findViewById(R.id.wifiLogo).setOnClickListener(new View.OnClickListener() {
 
@@ -61,13 +60,11 @@ public class SplashActivity extends Activity {
 
                     // Call openWifi method defined below
                     openWifi();
-
                 }
             });
         }
 
-        // If WiFi is turned on but is not connected to Network...press to open Available Networks
-
+        // If WiFi is turned on but is not connected to Network...press to open Available Networks and return back
         if (wifi.setWifiEnabled(true) && (userWifi.getState()) == NetworkInfo.State.DISCONNECTED) {
             TextView wifiMsg = (TextView) findViewById(R.id.wifiMsg);
             wifiMsg.setText("WIFI ENABLED BUT NOT ONLINE \r\n Click above to open WiFi Network Availability \r\n Or click below to Continue \r\n Data charges may apply when Submitting Testimony");
@@ -77,30 +74,21 @@ public class SplashActivity extends Activity {
                 @Override
                 public void onClick(View v) {
 
-                    finish();
-                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    Intent openWiFiintent = (new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    startActivityForResult(openWiFiintent, 0);
                     Toast.makeText(getBaseContext(), "Please connect to a hotspot",
                             Toast.LENGTH_LONG).show();
                 }
+
             });
-        }
 
-        // The option to continue with disclaimer
-
-        else {
-
+            // if they don't care about wifi data
             findViewById(R.id.enterBtn).setOnClickListener(new View.OnClickListener() {
-
                 @Override
                 public void onClick(View v) {
 
-                    enterBtn.setVisibility(View.VISIBLE);
-
-                    TextView wifiMsg = (TextView) findViewById(R.id.wifiMsg);
-                    wifiMsg.setText("This app may use your mobile data to submit Testimony.");
-
                     Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                    finish();
+                    // finish(); ...turned it off just in case they hit back button and want to connect to wifi
                     startActivity(i);
 
                 }
@@ -109,11 +97,20 @@ public class SplashActivity extends Activity {
     }
 
     // Open wifi method called from above
-
     private void openWifi() {
 
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifi.setWifiEnabled(true);
+
+    }
+
+    // Return from setting network and refresh Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Intent splashIntent = getIntent();
+        finish();
+        startActivity(splashIntent);
 
     }
 }
